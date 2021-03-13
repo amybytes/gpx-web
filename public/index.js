12350a16ed11e33f03f9bf2e5bc4448379e603aa
Ethan Rowan <erowan@uoguelph.ca>
2021-03-13 18:41:48 -0500

    Added some GUI interactions

--- public/index.js
+++ public/index.js
@@ -1,6 +1,14 @@
 // Put all onload AJAX calls here, and event listeners
 $(document).ready(function() {
     initializeAddRouteWaypoints();
+    let file = {"name": "test.gpx", "version": 1.1, "creator": "Ethan Rowan", "numWaypoints": 3, "numRoutes": 1, "numTracks": 1};
+    // setGPXViewFile(file);
+    addFileToFileLog(file);
+    let routes = [{"name": "testroute", "numPoints": 5, "len": 123, "loop": true},{"name": "testroute2", "numPoints": 7, "len": 456, "loop": false}];
+    let tracks = [{"name": "testtrack", "numPoints": 5, "len": 123, "loop": true},{"name": "testtrack2", "numPoints": 7, "len": 456, "loop": false}];
+    addComponentsToGPXView(file, routes, "Route");
+    addComponentsToGPXView(file, tracks, "Track");
+
     // On page-load AJAX Example
     $.ajax({
         type: 'get',            //Request type
@@ -74,11 +82,11 @@ $(document).ready(function() {
             //Create an object for connecting to another waypoint
         });
     });
-});
 
-document.getElementById("btnAddWpt").onclick = function() {
-    addWaypointElement();
-};
+    document.getElementById("btnAddWpt").onclick = function() {
+        addWaypointElement();
+    };
+});
 
 function addWaypointElement() {
     let grid = document.getElementById("addRouteWaypointGrid");
@@ -114,6 +122,14 @@ function createRemoveWaypointButton() {
     return input;
 }
 
+function createBasicButton(name) {
+    let input = document.createElement("input");
+    input.setAttribute("type", "button");
+    input.setAttribute("class", "btn btn-secondary gridInput");
+    input.setAttribute("value", name);
+    return input;
+}
+
 function initializeAddRouteWaypoints() {
     for (let i = 0; i < 2; i++) {
         addWaypointElement();
@@ -140,4 +156,75 @@ function showAddRouteAction() {
 
 function showFindPathAction() {
     showAction("#findPath");
+}
+
+function renameComponent(gpxFileName, componentName, type) {
+    console.log("Rename requested route/track '" + componentName + "' of '" + gpxFileName + "'");
+}
+
+function showOtherData(gpxFileName) {
+    console.log("Showing other data for '" + gpxFileName + "'");
+}
+
+function addFileToFileLog(file) {
+    let fileLogTable = document.getElementById("fileLogTable");
+    let row = document.createElement("tr");
+    let td = document.createElement("td");
+    let link = document.createElement("a");
+    let linkText = document.createTextNode(file["name"]);
+    link.setAttribute("href", file["name"]);
+    link.appendChild(linkText);
+    td.appendChild(link);
+    row.appendChild(td);
+    td = document.createElement("td");
+    td.innerHTML = file["version"];
+    row.appendChild(td);
+    td = document.createElement("td");
+    td.innerHTML = file["creator"];
+    row.appendChild(td);
+    td = document.createElement("td");
+    td.innerHTML = file["numWaypoints"];
+    row.appendChild(td);
+    td = document.createElement("td");
+    td.innerHTML = file["numRoutes"];
+    row.appendChild(td);
+    td = document.createElement("td");
+    td.innerHTML = file["numTracks"];
+    row.appendChild(td);
+    fileLogTable.appendChild(row);
+}
+
+function addComponentsToGPXView(file, components, type) {
+    let gpxViewTable = document.getElementById("gpxViewTable");
+    for (let i = 0; i < components.length; i++) {
+        let row = document.createElement("tr");
+        let td = document.createElement("td");
+        td.innerHTML = type + " " + (i+1);
+        row.appendChild(td);
+        td = document.createElement("td");
+        td.innerHTML = components[i]["name"];
+        row.appendChild(td);
+        td = document.createElement("td");
+        td.innerHTML = components[i]["numPoints"];
+        row.appendChild(td);
+        td = document.createElement("td");
+        td.innerHTML = components[i]["len"];
+        row.appendChild(td);
+        td = document.createElement("td");
+        td.innerHTML = components[i]["loop"];
+        row.appendChild(td);
+        td = document.createElement("td");
+        let btnRename = createBasicButton("Rename");
+        td.appendChild(btnRename);
+        btnRename.onclick = function(e) {
+            renameComponent(file["name"], components[i]["name"], type);
+        }
+        let btnViewOtherData = createBasicButton("View other data");
+        td.appendChild(btnViewOtherData);
+        btnViewOtherData.onclick = function() {
+            showOtherData(file["name"]);
+        };
+        row.appendChild(td);
+        gpxViewTable.appendChild(row);
+    }
 }
